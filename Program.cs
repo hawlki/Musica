9b5bfed410f00b5f045cfa3fc083f4013f249fb7
israel lopes <israel.lopes2710@gmail.com>
2021-02-28 00:55:48 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.Red;  
+            Console.WriteLine("------------");
+            Console.WriteLine("Bamboleo");
+            Console.WriteLine("Gypsy Kings");
+            Console.WriteLine("------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Este amor llega asi de esta manera");
+            Console.WriteLine("No tiene la culpa");
+            Console.WriteLine("Caballo de danza vana");
+            Console.WriteLine("Porque es muy despreciado, por eso");
+            Console.WriteLine("No te perdona de llorar");
+            Console.WriteLine();
+            Console.WriteLine("Este amor llega asi de esta manera");
+            Console.WriteLine("No tiene la culpa");
+            Console.WriteLine("Amor de compra e venta");
+            Console.WriteLine("Amor de en el pasado");
+            Console.WriteLine("Bem, bem, bem");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("No tenes perdón de Dios");
+            Console.WriteLine("Tu eres mi vida, la fortuna del destino");
+            Console.WriteLine("En el destino del desamparo");
+            Console.WriteLine("Lo mismo ya que ayer");
+            Console.WriteLine("Lo mismo soy yo");
+            Console.WriteLine();
+            Console.WriteLine("(No te encuentro a I'abandon)");
+            Console.WriteLine("Eres imposible no te encuentro de verdad");
+            Console.WriteLine("Por eso un día no encuentro si de nada");
+            Console.WriteLine("Lo mismo ya que ayer");
+            Console.WriteLine("Lo pienso en ti");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.WriteLine();
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.WriteLine("Bamboleo, bambolea");
+            Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
