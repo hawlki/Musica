3a35fbc328e49ab719775a95119b933bd40366b9
israel lopes <israel.lopes2710@gmail.com>
2021-03-21 16:10:04 -0300

    organiza arquivo

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ namespace musica
         {
             Console.Clear();
             Console.Beep();
+
             Console.ForegroundColor = ConsoleColor.Red;  
             Console.WriteLine("------------");
             Console.WriteLine("Bamboleo");
             Console.WriteLine("Gypsy Kings");
             Console.WriteLine("------------");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Este amor llega asi de esta manera");
             Console.WriteLine("No tiene la culpa");
@@ -21,18 +23,21 @@ namespace musica
             Console.WriteLine("Porque es muy despreciado, por eso");
             Console.WriteLine("No te perdona de llorar");
             Console.WriteLine();
+
             Console.WriteLine("Este amor llega asi de esta manera");
             Console.WriteLine("No tiene la culpa");
             Console.WriteLine("Amor de compra e venta");
             Console.WriteLine("Amor de en el pasado");
             Console.WriteLine("Bem, bem, bem");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("No tenes perdon de Dios");
             Console.WriteLine("Tu eres mi vida, la fortuna del destino");
@@ -40,33 +45,40 @@ namespace musica
             Console.WriteLine("Lo mismo ya que ayer");
             Console.WriteLine("Lo mismo soy yo");
             Console.WriteLine();
+
             Console.WriteLine("(No te encuentro a I abandon)");
             Console.WriteLine("Eres imposible no te encuentro de verdad");
             Console.WriteLine("Por eso un día no encuentro si de nada");
             Console.WriteLine("Lo mismo ya que ayer");
             Console.WriteLine("Lo pienso en ti");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine();
+
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine("Bamboleo, bambolea");
             Console.WriteLine("Porque mi vida, yo lo aprender a vivir asi");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Etec Adolpho Berezin");
             Console.WriteLine("Feito por Israel Camilo Lopes");
             Console.WriteLine("1I3");
             Console.WriteLine("-----------------------------");
+
             Console.ResetColor();
             Console.WriteLine();
+
             Console.WriteLine("pressione uma tecla para limpar a tela");
             Console.ReadKey();
+            
             Console.Clear();
         }
     }
